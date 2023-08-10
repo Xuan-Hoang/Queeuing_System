8225d7366efd4425cfcaac8059f2d0961f8e2d17
Do Xuan Hoang 20-0-00140 <hoangdx20.cdsg@saigontech.edu.vn>
2023-08-10 13:38:05 +0700

    Thêm layoyt Header

--- src/layout/Header/header.tsx
+++ src/layout/Header/header.tsx
@@ -5,12 +5,13 @@ import { AppDispatch, RootState } from '../../redux/store';
 import { setUser } from '../../redux/slice/login/loginSlice';
 import { Col, Image, Row } from 'antd';
 import bell from '../../assets/icon/bell.svg';
+import '../../assets/css/header.css';
+import { NavLink } from 'react-router-dom';
 const HeaderPage = () => {
   const user = useSelector((state: RootState) => state.auth.user);
   const dispatch: AppDispatch = useDispatch();
 
   useEffect(() => {
-    // Lấy thông tin người dùng từ Local Storage khi component được render
     const storedUser = localStorage.getItem('user');
     if (storedUser) {
       const userData = JSON.parse(storedUser);
@@ -21,17 +22,46 @@ const HeaderPage = () => {
   if (!user) {
     return <div>Please log in to view your profile.</div>;
   }
+  const currentPath = window.location.pathname;
+  let pContent = '';
+  if (currentPath === '/home/profile') {
+    pContent = 'Thông Tin Cá Nhân';
+  } else if (currentPath === '/home/dashboard') {
+    pContent = 'Dashboard';
+  } else if (currentPath === '/home/device') {
+    pContent = 'Thiết Bị';
+  } else if (currentPath === '/home/service') {
+    pContent = 'Dịch vụ';
+  } else if (currentPath === '/home/number_level') {
+    pContent = 'Cấp Số';
+  } else if (currentPath === '/home/report') {
+    pContent = 'Báo cáo';
+  } else if (currentPath === '/home/setting') {
+    pContent = 'Cài đặt hệ thống';
+  }
   return (
     <Header className='header' style={{ backgroundColor: 'white' }}>
-      <Row>
-        <Col span={19}>
-          <h3 style={{ textAlign: 'start' }}>Thông Tin Cá Nhân</h3>
+      <Row justify='center' align='middle'>
+        <Col span={18}>
+          <p className='header-title'>{pContent}</p>
         </Col>
         <Col span={1}>
-          <Image className='header-images' src={bell} />
+          <Image className='header-icon' src={bell} />
         </Col>
-        <Col span={4}>
-          <Image width={50} className='header-images' src={user.avata} />
+        <Col span={5}>
+          <div className='header-info'>
+            <div className='header-circle-container'>
+              <Image className='header-circle-image' src={user.avata} alt='Image' />
+            </div>
+            <div className='heder-info-text'>
+              <p className='heder-info-text1'>Xin Chao</p>
+              <p className='heder-info-text2'>
+                <NavLink className='heder-info-text2' to='/home/profile'>
+                  {user.fullName}
+                </NavLink>
+              </p>
+            </div>
+          </div>
         </Col>
       </Row>
     </Header>
